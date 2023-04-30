1be69bb4979e535b79b41a9e7c802b9fa150a127
morteza moradi <amirbhz86@gmail.com>
2023-04-30 16:01:54 +0330

    change

---
+++ android/build/generated/source/buildConfig/debug/com/rpt/reactnativecheckpackageinstallation/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.rpt.reactnativecheckpackageinstallation;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.rpt.reactnativecheckpackageinstallation";
+  public static final String BUILD_TYPE = "debug";
+}
